e9295110726123b7dadbc9121101f2a8fed53877
victoriajiang123 <victoriaijiang@gmail.com>
2026-07-13 21:59:37 -0400

    Implement native host connection and message handling

--- NRT_A2D/Background.js
+++ NRT_A2D/Background.js
@@ -1,1 +1,23 @@
+let port = null;
 
+function connectHost() {
+  port = chrome.runtime.connectNative('com.clicksave.host');
+
+  port.onMessage.addListener((response) => {
+    console.log('Native host response:', response);
+  });
+
+  port.onDisconnect.addListener(() => {
+    if (chrome.runtime.lastError) {
+      console.error('Native host disconnected:', chrome.runtime.lastError.message);
+    }
+    port = null;
+  });
+}
+
+chrome.runtime.onMessage.addListener((msg) => {
+  if (msg.action === 'saveData') {
+    if (!port) connectHost();
+    port.postMessage({ data: msg.data });
+  }
+});
